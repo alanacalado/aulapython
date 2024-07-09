8326764aded5b04b25b9c1856784b74524796f02
alanacalado <alanacalado1@gmail.com>
2024-07-09 08:11:42 -0300

    Add files via upload

---
+++ contagem.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+cont = int(input("Digite um número inteiro: "))
+
+while cont >=0:
+    os.system('cls') # Limpar o terminal
+    print(f'Contagem regressiva: {cont}...')
+    time.sleep(1) # atrasa o proximo comando
+    cont -= 1
+20
+
+os.system('cls')
+print('BOOOOOOOOMMMMMMM')
